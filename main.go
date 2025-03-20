467ed5475d4a95d412ffaac916731b82577d6894
b-isry <bisratt1995@gmail.com>
2025-03-20 10:13:03 +0300

    Initialize library management system with data structures and main menu

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"Library_Management/controllers"
+	"Library_Management/models"
+	"Library_Management/services"
+	"fmt"
+)
 
 func main() {
-
-	fmt.Println("========== Library Management System ==========")
-
+	library := &services.Library{
+		Books:   make(map[int]models.Books),
+		Members: make(map[int]models.Member),
 	}
-
+	fmt.Println("========== Library Management System ==========")
+	controllers.BookController(library)
+}
