438cc46b97a2cad236ed78a4b197311983f21ffd
b-isry <bisratt1995@gmail.com>
2025-03-20 10:19:28 +0300

    Import other modules

--- services/library_service.go
+++ services/library_service.go
@@ -1,110 +1,115 @@
 package services
 
 import (
+	"Library_Management/models"
 	"fmt"
-	"../models"
-	"../controllers"
 )
 
-type Library struct{
-	books map[int]models.Books
-	members map[int]models.Member
+type Library struct {
+	Books   map[int]models.Books
+	Members map[int]models.Member
 }
 
-type LibraryManager interface{
-	AddBook(book Books)
+type LibraryManager interface {
+	AddBook(book models.Books)
+	AddMember(member models.Member)
 	RemoveBook(bookId int)
 	BorrowBook(memberID int, bookID int)
 	ReturnBook(memberID int, bookID int)
-	ListAvailableBooks() []Books
-	ListBorrowedBooks() []Books
-	}
+	ListAvailableBooks() []models.Books
+	ListBorrowedBooks() []models.Books
+}
 
-func (l *Library) AddBook(book models.Books){
-	L.books[book.id] = book
+func (l *Library) AddBook(book models.Books) {
+	l.Books[book.Id] = book
+	book.Status = "available"
 	fmt.Println("Book added successfully")
 }
 
-func (l *Library) RemoveBook(bookId int){
-	delete(l.books, bookId)
+func (l *Library) AddMember(member models.Member) {
+	l.Members[member.Id] = member
+	fmt.Println("Member added successfully")
+}
+
+func (l *Library) RemoveBook(bookId int) {
+	delete(l.Books, bookId)
 	fmt.Println("Book removed successfully")
 }
 
-func (l *Library) BorrowBook(memberID int, bookID int){
-	member, ok := l.members[memberID]
-	if !ok{
+func (l *Library) BorrowBook(memberID int, bookID int) {
+	member, ok := l.Members[memberID]
+	if !ok {
 		fmt.Println("Member not found")
 		return
 	}
-	book, ok := l.books[bookID]
-	if !ok{
+	book, ok := l.Books[bookID]
+	if !ok {
 		fmt.Println("Book not found")
 		return
 	}
-	if book.status != "available"{
+	if book.Status != "available" {
 		fmt.Println("Book is not available")
 		return
 	}
-	book.status = "borrowed"
+	book.Status = "borrowed"
 	member.BorrowedBooks = append(member.BorrowedBooks, book)
-	l.books[bookID] = book
-	fmt.Println("Book borrowed successfully")
+	l.Books[bookID] = book
+
+	fmt.Printf(" %s borrowed %s successfully\n", member.Name, book.Title)
 }
 
-func (l *Library) ReturnBook(memberID int , bookID int){
-	member, ok := l.members[memberID]
-	if !ok{
+func (l *Library) ReturnBook(memberID int, bookID int) {
+	member, ok := l.Members[memberID]
+	if !ok {
 		fmt.Println("Member not found")
 		return
 	}
-	book, ok := l.books[bookID]
-	if !ok{
+	book, ok := l.Books[bookID]
+	if !ok {
 		fmt.Println("Book not found")
 		return
 	}
-	if book.status != "borrowed"{
+	if book.Status != "borrowed" {
 		fmt.Println("Book is not borrowed")
 		return
 	}
-	book.status = "available"
+	book.Status = "available"
 	member.BorrowedBooks = removeFromSlice(member.BorrowedBooks, book)
-	l.books[bookID] = book
-	fmt.Println("Book returned successfully")
+	l.Books[bookID] = book
+	fmt.Printf(" %s returned %s successfully\n", member.Name, book.Title)
 }
 
-func (l *Library) ListAvailableBooks() []models.Books{
-	availableBooks := []models.books
-	for _, book := range l.books{
-		if book.status == "available"{
+func (l *Library) ListAvailableBooks() []models.Books {
+	availableBooks := []models.Books{}
+	for _, book := range l.Books {
+		if book.Status == "available" {
 			availableBooks = append(availableBooks, book)
 		}
-	}
-	fmt.Println("Available books:")
-	for _, book := range availableBooks{
-		fmt.Println(book.title)
+	}	
+	for _, book := range availableBooks {
+		fmt.Println(book.Title)
 	}
 	return availableBooks
 }
 
 func (l *Library) ListBorrowedBooks() []models.Books {
 	borrowedBooks := []models.Books{}
-	for _, book := range l.books {
-		if book.status == "borrowed" {
+	for _, book := range l.Books {
+		if book.Status == "borrowed" {
 			borrowedBooks = append(borrowedBooks, book)
 		}
 	}
-	fmt.Println("borrowed books:")
-	for _, book := range borrowedBooks{
-		fmt.Println(book.title)
+	for _, book := range borrowedBooks {
+		fmt.Println(book.Title)
 	}
 	return borrowedBooks
 }
 
-func removeFromSlice(slice []models.Books, book models.Books) []models.Books{
-	for i, b := range slice{
-		if b.id == book.id{
+func removeFromSlice(slice []models.Books, book models.Books) []models.Books {
+	for i, b := range slice {
+		if b.Id == book.Id {
 			return append(slice[:i], slice[i+1:]...)
 		}
 	}
+	return slice
 }
-
